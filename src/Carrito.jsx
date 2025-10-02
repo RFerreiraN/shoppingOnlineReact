4ecd0498de33e73ca1b1149d83cc1855a8483f9d
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-10-02 22:22:12 +0200

    creamso barra de navegacion

--- src/Carrito.jsx
+++ src/Carrito.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import {useState} from 'react';
 
 export const Carrito = () => {
   return (
-    <div>Carrito</div>
+    <>
+      <div className="container">
+        <h2>Carrito</h2>
+      </div>
+    </>
   )
 }
