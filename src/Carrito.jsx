c2a566f55ba3d354e28c4b3b9b68a51f15a7d765
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-10-02 22:48:02 +0200

    navbar creada

--- src/Carrito.jsx
+++ src/Carrito.jsx
@@ -1,10 +1,13 @@
-import {useState} from 'react';
+import { useState } from 'react';
+import { NavBar } from './Components/NavBar';
+import { Route, Routes } from 'react-router-dom';
 
 export const Carrito = () => {
   return (
     <>
+      <NavBar></NavBar>
       <div className="container">
-        <h2>Carrito</h2>
+    
       </div>
     </>
   )
